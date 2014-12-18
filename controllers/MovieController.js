0d182b3e4ab5c51743921e556777a9092c0685f7
Alyssa <alyssaw90@gmail.com>
2014-12-18 14:47:32 -0800

    styling changes

--- controllers/MovieController.js
+++ controllers/MovieController.js
@@ -3,7 +3,7 @@ var mood = {
 	"happy": 35,
 	"adventure": 12, 
 	"drama": 18,
-	"suspense": 10748,
+	"indie": 10756,
 	"romance": 10749,
 	"family": 10751
 }
@@ -49,16 +49,16 @@ module.exports = {
 			res.render("adventure", {data:data, page:page, user:user})
 		})
 	},
-	get_suspense:function(req, res){
+	get_indie:function(req, res){
 		var user = req.getUser()
 		var page = req.query.page || 1;
-		var moviesSearchUrl ="http://api.themoviedb.org/3/genre/10748/movies?api_key=" + process.env.moviedata + "&page=" + page;
+		var moviesSearchUrl ="http://api.themoviedb.org/3/genre/10756/movies?api_key=" + process.env.moviedata + "&page=" + page;
 		request(moviesSearchUrl, function(error, response, body){
 			var info = JSON.parse(body);
 			var data = info.results.map(function(data){
 				return data;
 			});
-			res.render("suspense", {data:data, page:page, user:user})
+			res.render("indie", {data:data, page:page, user:user})
 		})
 	},
 	get_romance:function(req, res){
